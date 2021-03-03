9bd9e0dff2a0e451f1325a7a9838c716a6895101
vasilii <77785157+vasilii96@users.noreply.github.com>
2021-03-03 11:54:40 +0300

    создали и зарегистривали приложение в settings.py

--- alhbret/alhbret/settings.py
+++ alhbret/alhbret/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'medsite.apps.MedsiteConfig' # создали и зарегистривали приложение
 ]
 
 MIDDLEWARE = [
